3359983f47c7b38cbaca4eb950cf6aec0b6c8dc8
tom-mate-o <132936694+tom-mate-o@users.noreply.github.com>
2024-04-07 18:40:25 +0200

    wetter api

--- README.md
+++ README.md
@@ -1,70 +1,7 @@
-# Getting Started with Create React App
+# API-Project - waves, wind & wet weather?!
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+API link weather
+https://api.open-meteo.com/v1/forecast?latitude=53.0833&longitude=4.8333&hourly=temperature_2m,precipitation,wind_speed_10m&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,sunshine_duration,uv_index_max,precipitation_sum,precipitation_hours,wind_speed_10m_max,wind_direction_10m_dominant&timezone=Europe%2FBerlin&past_days=1&forecast_days=1
 
-## Available Scripts
-
-In the project directory, you can run:
-
-### `npm start`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
-
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+API link waves
+https://marine-api.open-meteo.com/v1/marine?latitude=53.0833&longitude=4.8333&hourly=wave_height&daily=wave_height_max,wind_wave_direction_dominant&timezone=Europe%2FBerlin&past_days=1&forecast_days=1
--- src/App.js
+++ src/App.js
@@ -1,9 +1,138 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(
+      'https://api.open-meteo.com/v1/forecast?latitude=53.0833&longitude=4.8333&hourly=temperature_2m,precipitation,wind_speed_10m&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,sunshine_duration,uv_index_max,precipitation_sum,precipitation_hours,wind_speed_10m_max,wind_direction_10m_dominant&timezone=Europe%2FBerlin&past_days=1&forecast_days=1'
+    )
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <div className="App">
       <h1>waves, wind & wet weather?!</h1>
+
+      <div className="locationContainer">
+        <p>Texel</p>
+        <p>53°04'59.9"N 4°49'59.9"E</p>
+      </div>
+
+      <div className="wavesContainer">
+        <h2 id="waves">waves</h2>
+        <div className="bubbleContainer">
+          <p>wave height - last & next 24h</p>
+          <div className="chartContainer">Chart</div>
+          <div className="dataContainer">
+            <div className="data">
+              <p>ICON</p>
+              <h3>3.12m</h3>
+              <p>max wave height ( last 24h )</p>
+            </div>
+            <div className="data">
+              <p>ICON</p>
+              <h3>East</h3>
+              <p>dominant wave direction ( last 24h )</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="windContainer">
+        <h2 id="wind">wind</h2>
+        <div className="bubbleContainer">
+          <p>wave height - last & next 24h</p>
+          <div className="chartContainer">Chart</div>
+          <div className="dataContainer">
+            <div className="data">
+              <p>ICON</p>
+              <h3>3.12m</h3>
+              <p>max wave height ( last 24h )</p>
+            </div>
+            <div className="data">
+              <p>ICON</p>
+              <h3>East</h3>
+              <p>dominant wave direction ( last 24h )</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="rainContainer">
+        <h2 id="rain">rain</h2>
+        <div className="bubbleContainer">
+          <p>wave height - last & next 24h</p>
+          <div className="chartContainer">Chart</div>
+          <div className="dataContainer">
+            <div className="data">
+              <p>ICON</p>
+              <h3>3.12m</h3>
+              <p>max wave height ( last 24h )</p>
+            </div>
+            <div className="data">
+              <p>ICON</p>
+              <h3>East</h3>
+              <p>dominant wave direction ( last 24h )</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="temperatureContainer">
+        <h2 id="temperature">temperature</h2>
+        <div className="bubbleContainer">
+          <p>wave height - last & next 24h</p>
+          <div className="chartContainer">Chart</div>
+          <div className="dataContainer">
+            <div className="data">
+              <p>ICON</p>
+              <h3>
+                max {data && data.daily && data.daily.temperature_2m_max[1]}°C
+              </h3>
+              <p>highest temperature ( last 24h )</p>
+            </div>
+            <div className="data">
+              <p>ICON</p>
+              <h3>
+                {' '}
+                min {data && data.daily && data.daily.temperature_2m_min[1]}°C
+              </h3>
+              <p>lowest temperature ( last 24h )</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="sunContainer">
+        <h2 id="sun">sun</h2>
+        <div className="dataContainer">
+          <div className="data">
+            <p>ICON</p>
+            <h3> {data && data.daily && data.daily.sunrise[1]}</h3>
+            <p>todays sunrise</p>
+          </div>
+          <div className="data">
+            <p>ICON</p>
+            <h3> {data && data.daily && data.daily.sunset[1]}</h3>
+            <p>todays sunset</p>
+          </div>
+          <div className="data">
+            <p>ICON</p>
+            {/* sekunden */}
+            <h3> {data && data.daily && data.daily.sunshine_duration[1]}</h3>
+            <p>todays sunshine duration</p>
+          </div>
+          <div className="data">
+            <p>ICON</p>
+            <h3> {data && data.daily && data.daily.uv_index_max[1]}</h3>
+            <p>todays UV-index</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
